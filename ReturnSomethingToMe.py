3448f023dae0a17f6aa2a2d1f3b5ab16dfcb1876
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-29 15:43:39 -0400

    Return Something to Me Completed Code
    Completed code with a bit of pseudocode.

---
+++ ReturnSomethingToMe.py
@@ -0,0 +1,3 @@
+# Write a function that returns the string "something" joined with a space " " and the given argument a.
+def give_me_something (a): # This is a definition of "give_me_something".
+	return ("something " + a) # You have to put whatever you are trying to return in parenthesis.
