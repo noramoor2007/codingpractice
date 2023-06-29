0951797060c654b7c8270f2a5bb82b559b1e5344
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2023-06-29 16:29:33 -0400

    Less Than 100 Completed Code
    Completed code with a bit of pseudocode.

---
+++ C002_LessThan100.py
@@ -0,0 +1,10 @@
+# Given two numbers, return True if the sum of both numbers is less than 100. Otherwise return False.
+def less_than_100 (a, b):
+	sum1 = a + b
+	trueorfalse = bool
+	if (sum1 < 100):
+		trueorfalse = True
+		return trueorfalse
+	else:
+		trueorfalse = False
+		return trueorfalse
